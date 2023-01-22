55eb97df2416b2b9b5b05ca1ba63f095204f1a3e
Thomas Dhollande <dhollande.thomas@gmail.com>
2023-01-22 02:45:16 +0100

    fix path, public_path and css js imports

--- views/common/header.php
+++ views/common/header.php
@@ -6,18 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($page_title) && $page_title !== '' ? $page_title : 'Smash' ?></title>
     <link rel="stylesheet" href="https://use.typekit.net/bsu4zoh.css">
-    <link rel="stylesheet" href="./public/style/main.css">
+    <link rel="stylesheet" href="<?= PUBLIC_PATH ?>style/main.css">
     <?php if (isset($css_file_name) && $css_file_name !== '') : ?>
-        <link rel="stylesheet" href="./public/style/<?= $css_file_name ?>.css">
+        <link rel="stylesheet" href="<?= PUBLIC_PATH ?>style/<?= $css_file_name ?>.css">
     <?php endif; ?>
 </head>
 
 <body>
     <nav class="menu solano-bold">
         <div class="menu-logo">
-            <a href="./"></a>
-            <img src="./public/images/logo/logo-smash-black.svg" alt="Logo Smash Black" class="logo-smash-black active">
-            <img src="./public/images/logo/logo-smash-white.svg" alt="Logo Smash White" class="logo-smash-white">
+            <a href="<?= PATH ?>"></a>
+            <img src="<?= PUBLIC_PATH ?>images/logo/logo-smash-black.svg" alt="Logo Smash Black" class="logo-smash-black active">
+            <img src="<?= PUBLIC_PATH ?>images/logo/logo-smash-white.svg" alt="Logo Smash White" class="logo-smash-white">
         </div>
         <div class="menu-right">
             <div class="menu-auth">
