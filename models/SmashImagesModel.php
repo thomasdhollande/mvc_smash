d5df997b2ab23557300c36b00771bcd63da4e10b
Eloicau <eloi.caudrillier@gmail.com>
2023-01-23 21:22:32 +0100

    Smash_images and Smash_videos class create + Smash_images model create

---
+++ models/SmashImagesModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace models;
+
+use models\base\SQL;
+use models\classes\SmashImages;
+
+class SmashImagesModel extends SQL
+{
+    public function __construct()
+    {
+        parent::__construct('smash_images', 'id');
+    }
+
+    /**
+     * Liste les images d'un jeu
+     * @param string $smash_id
+     * @return SmashImages[]
+     */
+    public function allSmashGameImages(string $smash_id): array
+    {
+        $query = "SELECT * FROM smash_images WHERE smash_id = ?";
+        $stmt = SQL::getPdo()->prepare($query);
+        $stmt->execute([$smash_id]);
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, SmashImages::class);
+    }
+
+    /**
+     * Ajoute une image en base pour le jeu $smash_id
+     * @param SmashImages $path
+     * @return string
+     */
+    public function createSmashImage(SmashImages $image): string
+    {
+        $query = "INSERT INTO smash_images (id, smash_id, path) VALUE (NULL, smash_id, ?)";
+        $stmt = SQL::getPdo()->prepare($query);
+        $stmt->execute([$image->getSmashId(),$image->getPath()]);
+        return $this->getPdo()->lastInsertId();
+    }
+}
--- views/SmashOneGameView.php
+++ views/SmashOneGameView.php
@@ -1,3 +1,6 @@
 <section id="smashOneGame" class="top-section">
 
+        <div><?= $smashOneGame->getName() ?></div>
+        <div><?= $smashOneGame->getDateRelease() ?></div>
+
 </section>
